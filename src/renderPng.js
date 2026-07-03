5d48d5f28d9b5d1306cdb2c6aa3c12ee4fdffb71
Olcha <bartoszolszowski@gmail.com>
2026-07-03 20:23:52 +0200

    save usage while generating png

--- src/renderPng.js
+++ src/renderPng.js
@@ -101,7 +101,7 @@ async function renderPng({
 
   const browser = await puppeteer.launch({
     executablePath,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
   });
 
   try {
